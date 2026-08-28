4134218330f78aed5240f9634bbf0ce519d2a54b
Muzammil <muzammil.ab314@gmail.com>
2026-08-28 13:13:59 +0500

    Subfeat: Added Multiple Objects and Updated Enviornment & Script

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject obstacle;
+    [SerializeField] private GameObject[] obstacles;
+    [SerializeField] private float spawnRange = 3.5f;
 
     private void Start()
     {
@@ -15,16 +16,26 @@ public class ObstacleSpawner : MonoBehaviour
         while (true)
         {
             // Wait a random amount of time between 1 and 2 seconds
-            yield return new WaitForSeconds(Random.Range(1f, 2f));
+            yield return new WaitForSeconds(Random.Range(0.3f, 0.6f));
 
             // Random X position relative to this object's position
-            float randomX = Random.Range(-4.5f, 4.5f);
+            float randomX = Random.Range(-spawnRange, spawnRange);
 
             Vector3 spawnPosition = transform.position;
             spawnPosition.x += randomX;
 
+            // Choose a random obstacle
+            GameObject obstacle = obstacles[Random.Range(0, obstacles.Length)];
+
+            // Random rotation on all axes
+            Quaternion randomRotation = Quaternion.Euler(
+                Random.Range(0f, 360f),
+                Random.Range(0f, 360f),
+                Random.Range(0f, 360f)
+            );
+
             // Spawn the obstacle
-            Instantiate(obstacle, spawnPosition, Quaternion.identity);
+            Instantiate(obstacle, spawnPosition, randomRotation);
         }
     }
 }
