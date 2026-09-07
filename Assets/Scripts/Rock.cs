4ba0c88e4889ec323eb7a4f7742182c7ff434c20
Muzammil <muzammil.ab314@gmail.com>
2026-09-07 11:49:42 +0500

    Feat; Added Rock SFX, VFX and Cooldown

--- Assets/Scripts/Rock.cs
+++ Assets/Scripts/Rock.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using Unity.Cinemachine;
 
@@ -7,6 +8,16 @@ public class Rock : MonoBehaviour
     private CinemachineImpulseSource impulseSource;
 
     [SerializeField] private float maxDistance = 20f;
+    [SerializeField] private float audioDistance = 10f;
+
+    [SerializeField] private float sfxCooldown = 1f;
+    [SerializeField] private float vfxCooldown = 1f;
+
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private ParticleSystem particleEffect;
+
+    private float lastSfxTime = -Mathf.Infinity;
+    private float lastVfxTime = -Mathf.Infinity;
 
     private void Awake()
     {
@@ -16,14 +27,55 @@ public class Rock : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        float intensity = CalculateImpulseIntensity();
+
+        GenerateImpulse(intensity);
+        PlayAudio();
+        PlayImpactParticles(collision);
+    }
+
+    private float CalculateImpulseIntensity()
+    {
         float distance = Vector3.Distance(
             transform.position,
             mainCamera.transform.position
         );
 
-        // 0 = far away, 1 = right next to camera
-        float intensity = 1f - Mathf.Clamp01(distance / maxDistance);
+        // 0 = at maxDistance or further, 1 = right next to camera
+        return 1f - Mathf.Clamp01(distance / maxDistance);
+    }
 
+    private void GenerateImpulse(float intensity)
+    {
         impulseSource.GenerateImpulseWithForce(intensity);
     }
+
+    private void PlayAudio()
+    {
+        float distance = Vector3.Distance(
+            transform.position,
+            mainCamera.transform.position
+        );
+
+        if (distance > audioDistance)
+            return;
+
+        if (Time.time - lastSfxTime < sfxCooldown)
+            return;
+
+        audioSource.Play();
+        lastSfxTime = Time.time;
+    }
+
+    private void PlayImpactParticles(Collision collision)
+    {
+        if (Time.time - lastVfxTime < vfxCooldown)
+            return;
+
+        particleEffect.transform.position = collision.contacts[0].point;
+        particleEffect.Play();
+
+        lastVfxTime = Time.time;
+    }
 }
+
